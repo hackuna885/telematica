7a719cd601bda58904a1457a4bb8d78e92de8472
Hackuna MSI <oliver885@hotmail.com>
2023-02-22 19:13:10 -0600

    22022023

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ initial-scale=1.0">
                                 include 'assets/inc/califica2022_3.inc';
                                 break;
                             case 'listas':
-                                include 'assets/inc/listas2022_3.inc';
+                                include 'assets/inc/listas2023_1.inc';
                                 break;
                             case 'especiales':
                                 include 'assets/inc/especiales.inc';
